394623fd7c5ac15feb1c9b74a7f4e617b7298647
Leead Jacobowitz <leead123@gmail.com>
2024-06-07 09:01:58 +0300

    initial skeleton for blog page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
+      <div className="post">
+        <img src="https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fxtsjf89kaq9s9j35b7qf.png" alt=""></img>
+        <h2>How to write a technical blog post</h2>      
+        <p>Technical skill and content creation are equally important for your software development or career. Creating technical content can make it easier to land jobs, get promoted, and influence the direction of our industry because it enables hiring managers and other tech leaders to take a peek into your brain and your thought process.</p>
+      </div>
+      <div className="post">
+        <img src="https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fxtsjf89kaq9s9j35b7qf.png" alt=""></img>
+        <h2>How to write a technical blog post</h2>      
+        <p>Technical skill and content creation are equally important for your software development or career. Creating technical content can make it easier to land jobs, get promoted, and influence the direction of our industry because it enables hiring managers and other tech leaders to take a peek into your brain and your thought process.</p>
+      </div>
+      <div className="post">
+        <img src="https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fxtsjf89kaq9s9j35b7qf.png" alt=""></img>
+        <h2>How to write a technical blog post</h2>      
+        <p>Technical skill and content creation are equally important for your software development or career. Creating technical content can make it easier to land jobs, get promoted, and influence the direction of our industry because it enables hiring managers and other tech leaders to take a peek into your brain and your thought process.</p>
+      </div>
+    </main>
   );
 }
-
-export default App;
