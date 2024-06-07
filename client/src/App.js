fda70c2a7dac00d154a1bcc8fb04401e4ad6cd55
Leead Jacobowitz <leead123@gmail.com>
2024-06-07 11:06:04 +0300

    changed structure to pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,18 @@ import Header from './Header';
 import Layout from './Layout';
 import Post from './Post';
 import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 
 function App() {
   return (
     <Routes>
       <Route path={'/'} element={<Layout />}>
-        <Route index element={
-          <Post />
-        }/>
-        <Route path={'/login'} element={
-          <div>login page</div>
-        }/>
+        <Route index element={<HomePage />}/>
+        <Route path={'/login'} element={<LoginPage />}/>
+        <Route path={'/register'} element={<RegisterPage />}/>
       </Route>
     </Routes>
     
---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,8 @@
+export default function RegisterPage(){
+    return (
+        <>
+            <div>This is the register Page</div>
+        </>    
+    )
+}
+   
