1657d9066fd8c695618a2928fabf192f7fc4bd75
Leead Jacobowitz <leead123@gmail.com>
2024-06-07 09:09:20 +0300

    changed Header and Post to seperate components

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,19 @@
 import './App.css';
+import Header from './Header'
+import Post from './Post'
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-         <img src="https://techtowords.com/wp-content/uploads/2023/06/image-17.png" alt=""></img>
-        </div>
-        <div className="texts">
-          <h2>How to write a technical blog post</h2>    
-          <p className="info">
-            <a href="" className="author">Rizèl Scarlett</a>  
-            <time>Posted on Jun 7, 2023 • Updated on Jun 10, 2023</time>
-          </p>  
-          <p className="summary">Technical skill and content creation are equally important for your software development or career. Creating technical content can make it easier to land jobs, get promoted, and influence the direction of our industry because it enables hiring managers and other tech leaders to take a peek into your brain and your thought process.</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://techtowords.com/wp-content/uploads/2023/06/image-17.png" alt=""></img>
-        </div>
-        <div className="texts">
-          <h2>How to write a technical blog post</h2>    
-          <p className="info">
-            <a href="" className="author">Rizèl Scarlett</a>  
-            <time>Posted on Jun 7, 2023 • Updated on Jun 10, 2023</time>
-          </p>  
-          <p className="summary">Technical skill and content creation are equally important for your software development or career. Creating technical content can make it easier to land jobs, get promoted, and influence the direction of our industry because it enables hiring managers and other tech leaders to take a peek into your brain and your thought process.</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img src="https://techtowords.com/wp-content/uploads/2023/06/image-17.png" alt=""></img>
-        </div>
-        <div className="texts">
-          <h2>How to write a technical blog post</h2>    
-          <p className="info">
-            <a href="" className="author">Rizèl Scarlett</a>  
-            <time>Posted on Jun 7, 2023 • Updated on Jun 10, 2023</time>
-          </p>  
-          <p className="summary">Technical skill and content creation are equally important for your software development or career. Creating technical content can make it easier to land jobs, get promoted, and influence the direction of our industry because it enables hiring managers and other tech leaders to take a peek into your brain and your thought process.</p>
-        </div>
-      </div>
-      
-    </main>
+    <Routes>
+      <Route index element={
+        <main>
+          <Header />
+          <Post />
+          
+        </main>
+      } />
+    </Routes>
   );
 }
 
---
+++ client/src/Post.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Post(){
+    return (
+        <div className="post">
+            <div className="image">
+                <img src="https://techtowords.com/wp-content/uploads/2023/06/image-17.png" alt=""></img>
+            </div>
+            <div className="texts">
+                <h2>How to write a technical blog post</h2>    
+                <p className="info">
+                    <a href="" className="author">Rizèl Scarlett</a>  
+                    <time>Posted on Jun 7, 2023 • Updated on Jun 10, 2023</time>
+                </p>  
+                <p className="summary">Technical skill and content creation are equally important for your software development or career. Creating technical content can make it easier to land jobs, get promoted, and influence the direction of our industry because it enables hiring managers and other tech leaders to take a peek into your brain and your thought process.</p>
+            </div>
+        </div>
+    );
+}
+
