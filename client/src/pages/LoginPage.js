a80894d58d05f41e5399ee84f2ac3d929f2a7a7d
Leead Jacobowitz <leead123@gmail.com>
2024-06-13 00:14:08 +0300

    added rediredt on successful login

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,10 +1,15 @@
-import { useState } from "react"
+import { useState } from "react";
+import { Navigate } from 'react-router-dom';
+
+
 export default function LoginPage(){
-    const [userName, setUserName] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
+
 
     const handleUserNameChange = (event) => {
-        setUserName(event.target.value);
+        setUsername(event.target.value);
     }
 
     const handlePasswordChange = (event) => {
@@ -13,20 +18,31 @@ export default function LoginPage(){
 
     async function login(event) {
         event.preventDefault();
-        await fetch('http://localhost/login', {
-            method: 'GET',
-            body: JSON.stringify({userName, password}),
+        const response = await fetch('http://localhost:4000/login', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
             headers: {'Content-Type' : 'application/json'},
-        })
+            credentials: 'include',
+        });
+        if(response.ok){
+            setRedirect(true);
+        }
+        else {
+            alert('wrong creditials');
+        }
     }
 
+    if (redirect) {
+        return <Navigate to={'/'} />
+    }
+    
     return (
         <form className="login" onSubmit={login}>
             <h1>Login</h1>
             <input 
                 type="text" 
                 placeholder="username"
-                value={userName}
+                value={username}
                 onChange={handleUserNameChange}
                 />
             <input 
