8707527c9d12731f7c73c2ea80ca1e0f728277ca
Leead Jacobowitz <leead123@gmail.com>
2024-06-13 00:04:25 +0300

    added jsonwebtoken to login authentication

--- api/index.js
+++ api/index.js
@@ -3,19 +3,43 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const jwt = require('jsonwebtoken');
 const PORT = 4000;
 
 // password hash:
 const bcrypt = require('bcryptjs');
 const salt = bcrypt.genSaltSync(10);
+const secret = 'sdfljkhsdfg767dsfgkjshdfg876dsajkhfgsd98g6ysd8f';
 
 //mw
-app.use(cors()); 
+app.use(cors({credentials: true, origin: 'http://localhost:3003'})); 
 app.use(express.json()); //alows to extract request body as json
 
 // connect to the mongoose db:
 mongoose.connect('mongodb+srv://leead123:SA1ZCrAag47Z3q13@cluster0.wlaow54.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
+// login endpoint
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username: username});
+    //comparing users password with the hashed password saved in the DB.
+    const passwordOK = bcrypt.compareSync(password, userDoc.password);
+    if(passwordOK){
+        // logged in
+        jwt.sign({username, id: userDoc._id}, secret, {}, (err, token) => {
+            if(err){
+                throw err;
+            }
+            res.cookie('token', token).json('ok');
+        });
+    }
+    else{
+        res.status(400).json('wrong credintials!')
+    }
+
+})
+
+// register endpoint
 app.post('/register', async (req, res) => {
     const {username, password} = req.body;
     try{
